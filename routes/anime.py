e22f1d5e7d767ef11013a15671a53d5bc4bc6250
Rodrigo <rodrigoalvarez449@gmail.com>
2022-07-29 22:45:04 -0500

    Latest Rel section added & 2 extra sites

--- routes/anime.py
+++ routes/anime.py
@@ -14,22 +14,22 @@ configs = {
             "path": "popular.html",
             "params": ["page"],
             "container": {
-                "element": "div", 
+                "element": "div",
                 "class": "img"
             },
             "title": [
                 {
-                    "tag": "a", 
+                    "tag": "a",
                     "class": "",
                     "attribute": "title",
-                    "settings": [ { "action": "trim", "status": True } ]
+                    "settings": [{"action": "trim", "status": True}]
                 }
             ],
             "episode_id": [
                 {
-                    "tag": "a", 
+                    "tag": "a",
                     "class": "",
-                    "attribute": "href", 
+                    "attribute": "href",
                     "settings": [{"action": "split", "separator": "/", "select": -1}]
                 }
             ],
@@ -45,6 +45,44 @@ configs = {
         "latest_releases_section": {
             "path": "page-recent-release.html",
             "params": ["page", "type"],
+            "container": {
+                "element": "ul",
+                "class": "items",
+                "children": {
+                    "tag": "li",
+                    "class": ""
+                }
+            },
+            "title": [
+                {
+                    "tag": "a",
+                    "class": "",
+                    "attribute": "title",
+                    "settings": [{"action": "trim", "status": True}]
+                }
+            ],
+            "episode_id": [
+                {
+                    "tag": "a",
+                    "class": "",
+                    "attribute": "href",
+                    "settings": [{"action": "split", "separator": "/", "select": -1}]
+                }
+            ],
+            "episode_image": [
+                {
+                    "tag": "img",
+                    "class": "",
+                    "attribute": "src"
+                }
+            ],
+            "episode_number": [
+                {
+                    "tag": "p",
+                    "class": "episode",
+                    "settings": [{"action": "getText"}, {"action": "trim", "status": True}]
+                }
+            ]
         },
         "anime_details_section": {
 
@@ -57,13 +95,47 @@ configs = {
         }
     },
     "animeflv": {
-        "base_url": "https://www3.animeflv.net",
+        "base_url": "https://www.animeflv.one",
         "languages": ["sub-spanish", "dub-spanish"],
-        "popular_section": {
-
-        },
         "latest_releases_section": {
-
+            "path": "/",
+            "container": {
+                "element": "div",
+                "class": "ul hm",
+                "children": {
+                    "tag": "article",
+                    "class": "li"
+                }
+            },
+            "title": [
+                {
+                    "tag": "span",
+                    "class": "",
+                    "settings": [{"action": "getText"}, {"action": "trim", "status": True}]
+                }
+            ],
+            "episode_id": [
+                {
+                    "tag": "a",
+                    "class": "",
+                    "attribute": "href",
+                    "settings": [{"action": "split", "separator": "/", "select": -1}]
+                }
+            ],
+            "episode_image": [
+                {
+                    "tag": "img",
+                    "class": "",
+                    "attribute": "data-src"
+                }
+            ],
+            "episode_number": [
+                {
+                    "tag": "u",
+                    "class": "",
+                    "settings": [{"action": "getText"}, {"action": "trim", "status": True}]
+                }
+            ]
         },
         "anime_details_section": {
 
@@ -74,24 +146,100 @@ configs = {
         "search_anime_section": {
 
         }
+    },
+    "animeninja": {
+        "base_url": "https://www1.animeonline.ninja",
+        "languages": ["sub-spanish", "dub-spanish", "japanese"],
+        "latest_releases_section": {
+            "path": "episodio",
+            "container": {
+                "element": "div",
+                "class": "animation-2 items",
+                "children": {
+                    "tag": "article",
+                    "class": "episodes"
+                }
+            },
+            "title": [
+                {
+                    "tag": "center",
+                    "class": ""
+                },
+                {
+                    "tag": "div",
+                    "class": "data"
+                },
+                {
+                    "tag": "h3",
+                    "class": "",
+                    "settings": [{"action": "getText"}, {"action": "trim", "status": True}]
+                }
+            ],
+            "episode_id": [
+                {
+                    "tag": "div",
+                    "class": "poster"
+                },
+                {
+                    "tag": "div",
+                    "class": "season_m animation-1"
+                },
+                {
+                    "tag": "a",
+                    "class": "",
+                    "attribute": "href",
+                    "settings": [{"action": "split", "separator": "/", "select": -2}]
+                }
+            ],
+            "episode_image": [
+                {
+                    "tag": "div",
+                    "class": "poster"
+                },
+                {
+                    "tag": "img",
+                    "class": "lazy",
+                    "attribute": "data-src"
+                }
+            ],
+            "episode_number": [
+                {
+                    "tag": "h4",
+                    "class": "",
+                    "settings": [{"action": "getText"}, {"action": "trim", "status": True}]
+                }
+            ]
+        }
     }
 }
 
 
 router = APIRouter(prefix="/api/v1/anime")
 
+def exec_settings_in_tag(data_in, settings = []):
+    for setting in settings:
+        if setting["action"] == "getText":
+            data_in = data_in.getText()
+        if setting["action"] == "trim":
+            data_in = data_in.strip()
+        elif setting["action"] == "split":
+            data_in = data_in.split(setting["separator"])
+            data_in = data_in[setting["select"]]
+    
+    return data_in
+
 def dig_tag(root, elements):
     data = None
     for element in elements:
-        data = root.find(element["tag"], { "class": element["class"] })[element["attribute"]]
-        if data and "settings" in element:
-            for setting in element["settings"]:
-                if setting["action"] == "trim":
-                    data = data.strip()
-                elif setting["action"] == "split":
-                    data = data.split(setting["separator"])
-                    data = data[setting["select"]]
-                
+        #print("Searching tag: {} with class {}".format(element["tag"], element["class"]))
+        data = root.find(element["tag"], {"class": element["class"]})
+        #print("Data obtained: {}".format(data))
+        if "attribute" in element:
+            data = data[element["attribute"]]
+
+    if data and "settings" in element:
+        data = exec_settings_in_tag(data, element["settings"])
+    
     return data
 
 
@@ -101,10 +249,17 @@ def dig_tag(root, elements):
 @router.get("/{page}/popular")
 def get_popular_list(page: str, request: Request):
     if page not in configs:
-        return JSONResponse({ "error": "El servicio aun no ha sido agregado, por favor intenta con otro" }, 400)
-    
+        return JSONResponse({"error": "El servicio aun no ha sido agregado, por favor intenta con otro"}, 400)
+
     site_config = configs[page]
     base_url = site_config["base_url"]
+    if "popular_section" not in site_config:
+        return JSONResponse(
+            {
+                "error":"La seccion a la que se está intentando acceder no ha sido configurada o no existe en el sitio"
+            },
+            500)
+
     popular_config = site_config["popular_section"]
     url = "{}/{}".format(base_url, popular_config["path"])
 
@@ -119,19 +274,20 @@ def get_popular_list(page: str, request: Request):
 
         dig_in = popular_config["container"]
 
-        elements_list = site_soup.find_all( dig_in["element"], { "class": dig_in["class"] } )
+        elements_list = site_soup.find_all(
+            dig_in["element"], {"class": dig_in["class"]})
 
         if len(elements_list) == 0:
-            return JSONResponse({ "error": "No se ha encontrado ningun elemento en la seccion configurada. Por favor cambie el esquema de busqueda" }, 500)
+            return JSONResponse({"error": "No se ha encontrado ningun elemento en la seccion configurada. Por favor cambie el esquema de busqueda"}, 500)
 
-        #TODO: Implementar la busqueda de paginacion
+        # TODO: Implementar la busqueda de paginacion
         results = []
         for elem in elements_list:
             episode_title = dig_tag(elem, popular_config["title"])
             episode_id = dig_tag(elem, popular_config["episode_id"])
             episode_image = dig_tag(elem, popular_config["episode_image"])
             episode_number = dig_tag(elem, popular_config["episode_number"])
-            results.append({ 
+            results.append({
                 "link_id": episode_id,
                 "title": episode_title,
                 "image": episode_image,
@@ -143,43 +299,53 @@ def get_popular_list(page: str, request: Request):
         print(ex)
         return JSONResponse({"error": "Ha ocurrido un error al obtener el contenido"}, 500)
 
+
 @router.get("/{page}/latest")
 def get_latest_releases(page: str, request: Request):
-    pass
+    if page not in configs:
+        return JSONResponse({"error": "El servicio aun no ha sido agregado, por favor intenta con otro"}, 400)
 
-@router.get("/latest-releases")
-def get_latest(page: Optional[int] = 1):
-    results = []
-    url = "{}/page-recent-release.html".format(ANIME_BASE_URL)
-    gogo_res = requests.get(url, params={"page": page, "type": 1})
+    site_config = configs[page]
+    base_url = site_config["base_url"]
+    lrs_config = site_config["latest_releases_section"]
+    url = "{}/{}".format(base_url, lrs_config["path"])
 
-    if gogo_res.status_code != 200:
-        return JSONResponse({"error": "No se ha podido conectar con el servidor"}, gogo_res.status_code)
+    query_params = {}
+    if "params" in lrs_config:
+        for param in lrs_config["params"]:
+            if param in request.query_params._dict:
+                query_params[param]: request.query_params.get(param)
 
+    site_res = requests.get(url, params=query_params)
     try:
-        soup = bs(gogo_res.text, 'html.parser')
+        site_soup = bs(site_res.text, "html.parser")
+        list_tag = lrs_config["container"]["element"]
+        list_class = lrs_config["container"]["class"]
+        children_tag = lrs_config["container"]["children"]["tag"]
+        children_class = lrs_config["container"]["children"]["class"]
 
-        recent_added_list = soup.find("ul", {"class": "items"}).find_all("li")
+        recent_added_list = site_soup.find(list_tag, { "class": list_class }).find_all(children_tag, { "class": children_class })
+        
+        if len(recent_added_list) == 0:
+            return JSONResponse({
+                "error": "No se ha encontrado ningun elemento en la sección configurada. Por favor cambie el esquema de busqueda"
+            }, 500)
 
-        has_next_req = requests.get(url, params={"page": page + 1})
-
-        has_next = True
-
-        if has_next_req.status_code == 200:
-            next_sp = bs(has_next_req.text, 'html.parser')
-            has_next = len(next_sp.find(
-                "ul", {"class": "items"}).find_all("li")) != 0
-        else:
-            has_next = False
+        results = []
 
         for rec in recent_added_list:
-            title = rec.find("a")["title"]
-            ep_id = rec.find("a")["href"].split("/").pop()
-            image = rec.find("img")["src"]
-            ep_no = rec.find("p", {"class": "episode"}).getText()
-            results.append({"title": title, "id": ep_id,
-                           "image": image, "ep_no": ep_no})
-        return JSONResponse({"data": results, "has_next": has_next, "total": len(results)}, 200)
+            episode_title = dig_tag(rec, lrs_config["title"])
+            episode_id = dig_tag(rec, lrs_config["episode_id"])
+            episode_image = dig_tag(rec, lrs_config["episode_image"])
+            episode_number = dig_tag(rec, lrs_config["episode_number"])
+            results.append({
+                "link_id": episode_id,
+                "title": episode_title,
+                "image":  episode_image,
+                "number": episode_number
+            })
+
+        return JSONResponse({"data": results, "total": len(results)}, 200)
     except Exception as ex:
         print(ex)
         return JSONResponse({"error": "Ha ocurrido un error al obtener el contenido"}, 500)
